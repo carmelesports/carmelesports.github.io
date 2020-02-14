7ab47b703d96981bae66690f36051ee717dc3067
Matthew C. Gardner <matthewgardner@ccs.k12.in.us>
2020-02-14 11:00:06 -0500

    teams

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 function switchGame(id) {
-  document.getElementsByClassName("show")[0].classList.remove("show");
-  document.getElementById(id).classList.add("show");
+  var newGame = document.getElementById(id);
+  var oldGame = document.getElementsByClassName("activeYear")[0];
+  oldGame.classList.remove("activeYear");
+  oldGame.classList.add("notActive");
+  newGame.classList.remove("notActive");
+  newGame.classList.add("activeYear");
+}
+
+function changeYear(year, dir) {
+	var newYear = document.getElementById(year);
+	var oldYear = document.getElementsByClassName("activeYear")[0];
+  oldYear.classList.remove("activeYear");
+	oldYear.classList.add("notActive");
+	newYear.classList.remove("notActive");
+	newYear.classList.add("activeYear");
 }
