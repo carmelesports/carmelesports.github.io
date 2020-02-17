cec6f283726a48b6d8a819c1f10189560391d455
= <boilermonster0530@yahoo.com>
2020-02-16 22:38:48 -0500

    dropdown for games on mobile

--- main.js
+++ main.js
@@ -6,12 +6,3 @@ function switchGame(id) {
   newGame.classList.remove("notActive");
   newGame.classList.add("activeYear");
 }
-
-function changeYear(year, dir) {
-	var newYear = document.getElementById(year);
-	var oldYear = document.getElementsByClassName("activeYear")[0];
-  oldYear.classList.remove("activeYear");
-	oldYear.classList.add("notActive");
-	newYear.classList.remove("notActive");
-	newYear.classList.add("activeYear");
-}
