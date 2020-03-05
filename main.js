ef314b027986c4cfda7a9939bd1fc7bcb3709891
= <boilermonster0530@yahoo.com>
2020-03-04 21:07:15 -0500

    nav works better

--- main.js
+++ main.js
@@ -6,3 +6,23 @@ function switchGame(id) {
   newGame.classList.remove("notActive");
   newGame.classList.add("activeYear");
 }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  myFunction()
+};
+
+// Get the header
+var header = document.getElementById("theDiv");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop + .5 * header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    document.getElementById("nav").classList.add("borderBot");
+  } else {
+    document.getElementById("nav").classList.remove("borderBot");
+  }
+}
