e2ee3013da566c2e822623e63ba6d3db7477f6d3
MonsteRico <boilermonster0530@yahoo.com>
2020-03-10 11:04:43 -0400

    aowgihe

--- main.js
+++ main.js
@@ -16,10 +16,7 @@ window.onscroll = function() {
 var header = document.getElementById("theDiv");
 
 // Get the offset position of the navbar
-var sticky = header.offsetTop + .5 * header.offsetTop;
-if (header.classList.contains("home")) {
-  sticky = header.offsetTop;
-}
+var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
