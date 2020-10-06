46d2f9d9fbff897be4514a8c6d80ca5c2e228a3b
= <boilermonster0530@yahoo.com>
2020-10-05 23:47:05 -0400

    discord bot stuff

---
+++ results.js
@@ -0,0 +1,77 @@
+$.getJSON("../ScoreBot/results.json", function(data) {
+  console.log("retrieving scores");
+  $.each(data, function(i, score) {
+    var game = score.game;
+    var gameInt = gameInterpret(game);
+    var team1 = score.team1 + " " + gameInt;
+    var team2 = score.team2;
+    var team1Score = score.team1Score;
+    var team2Score = score.team2Score;
+    var date = score.date;
+    var now = moment();
+    var input = moment(date);
+    var isThisWeek = (now.isoWeek() == input.isoWeek())
+    var gameDiv = document.getElementById(game + "Div");
+    if (isThisWeek) {
+      gameDiv.innerHTML += ('<div class="col-md-5 matchCard">\
+        <br>\
+        <div class="row">\
+          <p class="team col-4">' + team1 + '</p>\
+          <p class="score col-4">vs</p>\
+          <p class="team col-4">' + team2 + '</p>\
+        </div>\
+        <div class="row text-wrap">\
+          <p class="score col-4">' + team1Score + '</p>\
+          <p class="score col-4">—</p>\
+          <p class="score col-4">' + team2Score + '</p>\
+        </div>\
+        <div class="row">\
+          <p class="col-12">' + input.format("MM/DD/yyyy") + '</p>\
+        </div>\
+      </div>');
+    }
+  });
+});
+
+function gameInterpret(game) {
+  var ret = null;
+  switch (game) {
+    case "Overwatch":
+      ret = "Overwatch";
+      break;
+    case "R6PC":
+      ret = "R6 PC";
+      break;
+    case "Hearthstone":
+      ret = "Hearthstone";
+      break;
+    case "CSGO":
+      ret = "CS:GO";
+      break;
+    case "LoL":
+      ret = "League of Legends";
+      break;
+    case "SSBU":
+      ret = "Smash";
+      break;
+    case "RL":
+      ret = "Rocket League";
+      break;
+    case "Forza":
+      ret = "Forza";
+      break;
+    case "CoD":
+      ret = "Call of Duty";
+      break;
+    case "Valorant":
+      ret = "Valorant";
+      break;
+    case "R6X1":
+      ret = "R6 X1";
+      break;
+    case "Apex":
+      ret = "Apex Legends";
+      break;
+  }
+  return ret;
+}
